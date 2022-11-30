b0de750b4c2bef3efc965e835f3d681a7ef8a85d
Angel01s <angelamayacruz203@gmail.com>
2022-11-30 16:13:50 -0500

    V1

--- index.php
+++ index.php
@@ -4,27 +4,25 @@
     $api = new ApiParte();
 
     if(isset($_GET['NE'])){
+        
         $NE = $_GET['NE'];
+        $api->getByIdParte($NE);
 
-        if(is_numeric($NE)){
-            $api->getByIdParte($NE);
-        }else{
-            $api->error('El NE es incorrecto');
-        }
     }if(isset($_GET['CODIGO'])){
-        $CODIGO = $_GET['CODIGO'];
 
-        if(is_numeric($CODIGO)){
-            $api->getByIdUsuario($CODIGO);
-        }else{
-            $api->error('El CODIGO es incorrecto');
-        }
+        $CODIGO = $_GET['CODIGO'];
+        $api->getByIdUsuario($CODIGO);   
+
     }elseif(isset($_GET['parte'])) {
+
         $parte = $_GET['parte'];
         $api->addParte($parte);
+
     }elseif(isset($_GET['usuario'])) {
+
         $usuario = $_GET['usuario'];
         $api->addUsuario($usuario);
+
     }
     else{
         $api->getAllPartes();
